06e6030ade227f144e032009b2b3000b28d5c057
Maocaroni <mcjagger35@gmail.com>
2026-02-25 12:44:01 -0500

    Update PlayerMove.cs

--- WTF/Assets/_Scripts/Plataformer/PlayerMove.cs
+++ WTF/Assets/_Scripts/Plataformer/PlayerMove.cs
@@ -11,6 +11,7 @@ public class PlayerMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        PlayerStats.score = 0;
         _newInput = GetComponent <NewInput>();
         _rb = GetComponent<Rigidbody2D>();
     }
