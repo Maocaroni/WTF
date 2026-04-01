6980c1bcd50272070c1b5072367c6881dc6696bd
Maocaroni <mcjagger35@gmail.com>
2026-03-31 20:15:22 -0500

    sa

--- WTF/Assets/_Scripts/Plataformer/PlayerAttack.cs
+++ WTF/Assets/_Scripts/Plataformer/PlayerAttack.cs
@@ -4,28 +4,34 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-
     private Rigidbody2D _rb;
     public float bounceForce;
 
-    // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-
     }
 
-  private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Enemy"))
         {
             if(_rb.velocity.y < 0)
             {
-              _rb.AddForce(transform.up * bounceForce, ForceMode2D.Impulse);
-              collision.GetComponent<EnemyMovement>().canMove = false;
-              Destroy(collision.gameObject, 1);
+                _rb.AddForce(transform.up * bounceForce, ForceMode2D.Impulse);
+                
+                // --- AQUÍ AÑADIMOS LA ANIMACIÓN ---
+                collision.GetComponent<Animator>().SetTrigger("Die");
+                // ----------------------------------
+
+                collision.GetComponent<EnemyMovement>().canMove = false;
+                
+                // Desactivamos el collider para que el personaje no siga chocando 
+                // con el enemigo mientras este hace la animación de morir.
+                collision.enabled = false; 
+
+                Destroy(collision.gameObject, 1);
             }
-       
         }
     }
 }
