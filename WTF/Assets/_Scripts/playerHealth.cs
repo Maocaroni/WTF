53758411b259631771d04140fe39d060b7043107
Maocaroni <mcjagger35@gmail.com>
2026-04-08 12:22:23 -0500

    sa

---
+++ WTF/Assets/_Scripts/playerHealth.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHealth : MonoBehaviour
+{
+    public int maxHealth;
+    private int currentHealth;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        animator = GetComponent <Animator> (); 
+    }
+
+    IEnumerator WaitForDamage()
+    {
+        Physics2D.IgnoreLayerCollision(3, 6, true);
+        currentHealth --;
+        animator.SetBool("IsHit", true);
+        yield return new WaitForSeconds(2f);
+        Physics2D.IgnoreLayerCollision(3, 6, false);
+        animator.SetBool("IsHit", false);
+    }
+
+
+    public void GetDamage()
+    {
+        if (currentHealth > 1)
+        {
+            StartCoroutine(WaitForDamage());
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+       
+
+}
